22326a9adfdeab6941c7d21d6015a229c9b9507c
Linh Pham <thuytpham9@gmail.com>
2019-11-05 06:42:32 +0000

    Fixed SQL tables

--- model.py
+++ model.py
@@ -20,15 +20,21 @@ class User(db.Model):
         return f"<User {user_id=self.user_id} {first_name=self.fname} {last_name=self.lname}>"
 
 class Company(db.Model):
+    """Create companies table"""
 
     __tablename__ = 'companies'
 
 
     ticker = db.Column(db.String(10), primary_key = True, unique=True)
     name = db.Column(db.String(10), nullable=False)
+
+    def_repr_(self):
+
+        return f"<Company ticker={self.ticker} company_name={self.name}>"
     
 
 class User_Company(db.Model):
+    """Create user_comapny tables to establish relationship"""
 
     __tablename__ = 'user_companies'
 
@@ -36,28 +42,36 @@ class User_Company(db.Model):
     user_id = db.Column(db.Interger, db.ForeignKey('users.user_id'), index=True)
     ticker = db.Column(db.String(10),db.ForeignKey('companies.ticker'), index=True)
 
+    #Define the relationship to User
+
+    user = db.relationship('User', backref = "user_companies")
+
+    company = db.relationship('Company', backref = "user_companies")
+
+
+    def_repr_(self):
+
+    return f"<User_Company main_id={self.main_id} user_id={self.user_id} ticker={self.ticker}>"
+
 class Daily_Price(db.Model):
+    """Create prices tables """
 
     __tablename__ = 'prices'
 
     price_id = db.Column(db.Interger, autoincrement=True, primary_key=True)
     ticker = db.Column(db.String(10), db.ForeignKey('companies.ticker'), unique=True)
-    date = 
+    date = db.Column(db.DateTime)
     open_p = db.Column(db.Interger, nullable = True)
     high_p = db.Column(db.Interger, nullable = True)
     low_p = db.Column(db.Interger, nullable = True)
     close_p = db.Column(db.Interger, nullable = True)
     volume_p = db.Column(db.Interger, nullable = True)
 
+    company = db.relationship('Company', backref = "prices")
 
+    def_repr_(self): 
 
-
-
-
-
-
-
-
+    return f"<Daily_Price close_p={self.close_p} volume_p={self.volume_p}>"
 
 
 ##############################################################################
@@ -67,9 +81,9 @@ def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PostgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///database'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_ECHO'] = False
     db.app = app
     db.init_app(app)
 
