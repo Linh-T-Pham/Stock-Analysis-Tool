a072a4b4a0292c94b830bd24168f1c282a979029
Linh Pham <thuytpham9@gmail.com>
2019-11-06 02:17:07 +0000

    Created seed.py

--- project.py
+++ project.py
@@ -2,18 +2,44 @@ import requests
 import json
 
 
-def home_request_api():
+def home_request_api(symbol):
 
 
-    api_1 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BAsub&outputsize=full&apikey=HPTIEFWDT4660PXF")
+    api = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=full&apikey=HPTIEFWDT4660PXF".format(symbol))
 
-    print(api_1.json())
-    return api_1.json()
+    company_api = api.json()
 
 
-    # api_2 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=full&apikey=HPTIEFWDT4660PXF")
+    """Get 5 years of daily_price data from 2014-11-01 to present"""
 
-    # print(api_2.json())
+    # for date, value in company_api["Time Series (Daily)"].items():
+
+    #     if date > "2013-12-5":
+    #         print(f"{date} : {value}")
+
+
+
+
+
+def load_DailyPrice():
+    """Load stock daily prices from API into databases."""
+
+    """Get 5 years of daily_price data from 2014-11-01 to present"""
+
+    for date, value in company_api["Time Series (Daily)"].items():
+
+        if date > "2013-12-5":
+            print(f"{date} : {value}")
+
+            daily_price = Daily_Price(ticker=ticker,
+                                     open_p=open_p,
+                                     low_p=low_p,
+                                     high_p=high_p,
+                                     close_p=close_p,
+                                     volume=volume)
+    
+
+    # List of stocks
     # MSFT : Microsoft 
     # PYPl : PayPal 
     # NVDA : Nvidia 
