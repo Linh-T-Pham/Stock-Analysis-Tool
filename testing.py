481e9a68de487331ebec3ef5819c01e7f3f38503
Linh Pham <thuytpham9@gmail.com>
2019-11-27 19:01:25 +0000

    Tested / route

---
+++ testing.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+from server import app
+from model import connect_to_db, db, User, Company, User_Company, DailyPrice
+# from flask import session 
+# import os 
+# import tempfile
+# import pytest 
+# from flaskr import flaskr 
+
+
+class FlaskTests(TestCase):
+    def setUp(self):
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+
+        result = self.client.get("/")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"<label>Enter a ticker", result.data)
+
+class 
+
+# class FlaskTestsDatabase(TestCase):
+#     def setUp(self):
+#         self.client = app.test_client()
+#         app.config['TESTING'] = True
+#         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#         connect_to_db(app, db_uri='postgresql:///testdb')
+
+#         db.create_all()
+#         #currenty example_data doesn't exist
+#         example_data() #write function in model.py that creates test data for each table
+
+#         def tearDown(self):
+#             db.session.close()
+#             db.drop_all()
+
+#         #write tests here
+
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
