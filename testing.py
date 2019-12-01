9544dd7dcf886ff2b6b9cb4a49103d2abdf72a67
Linh Pham <thuytpham9@gmail.com>
2019-12-01 22:18:15 +0000

    Tested and passed 3 more routes

--- testing.py
+++ testing.py
@@ -5,8 +5,6 @@ from server import app
 from model import connect_to_db, db, User, Company, User_Company, DailyPrice
 
 
-
-
 class FlaskTests(TestCase):
     def setUp(self):
 
@@ -19,22 +17,38 @@ class FlaskTests(TestCase):
 
         result = self.client.get("/")
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b"<label>Enter a ticker", result.data)
+        self.assertIn(b"<h1>Hello</h1>", result.data)
 
     def user(self):
+        """Test user_stock route"""
 
         result = self.client.get("/user_stock")
         self.assertEqual(result.status_code,200)
-        self.assertIn(b"")
+        self.assertIn(b"<h3>Current Market Data</h3>", result.data)
 
     def test_correlation(self):
-        """Test user_stock route"""
+        """Test correlation.json route"""
 
         result = self.client.get('/correlation.json')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'<h3>Correlation Analysis</h3>', result.data)
 
-# class FlaskTestsDatabase(TestCase):
+    def test_correlation(self):
+        """Test sector route"""
+
+        result = self.client.get('/sector')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<title>Sector Performance</title>', result.data)
+
+    def test_correlation(self):
+        """Test add_stock route"""
+
+        result = self.client.get('/add_stock')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<label>Enter a ticker', result.data)
+
+
+# # class FlaskTestsDatabase(TestCase):
 #     def setUp(self):
 #         self.client = app.test_client()
 #         app.config['TESTING'] = True
